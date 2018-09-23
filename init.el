55c548c90179c9d4c49ca3aa3932e5a2fbe5dae3
Aaron Culich <aculich@gmail.com>
2018-09-23 11:51:48 -0700

    don't exclude undo-tree

--- init.el
+++ init.el
@@ -93,7 +93,7 @@ values."
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
-   dotspacemacs-excluded-packages '(undo-tree ac-ispell)
+   dotspacemacs-excluded-packages '(ac-ispell)
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
    ;; `used-only' installs only explicitly used packages and uninstall any
