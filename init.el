0bc25d7c2a34ed5928477b3c9f062035d8430215
Aaron Culich <aculich@gmail.com>
2019-07-17 10:24:30 -0700

    load magithub as an additional package

--- init.el
+++ init.el
@@ -131,7 +131,7 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(osx-browse ssh-config-mode sx gitter visual-fill-column company-terraform noflet kubernetes kubernetes-evil kubernetes-tramp jekyll-modes octopress easy-hugo easy-jekyll easy-kill easy-kill-extras polymode poly-R poly-markdown)
+		dotspacemacs-additional-packages '(osx-browse ssh-config-mode sx gitter visual-fill-column company-terraform noflet kubernetes kubernetes-evil kubernetes-tramp jekyll-modes octopress easy-hugo easy-jekyll easy-kill easy-kill-extras polymode poly-R poly-markdown magithub)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
